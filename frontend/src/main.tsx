45b48e1430200a5701200ee9a4359fd6d781d594
Matheus Antunes Reis <123640371+matheusantunes-dev@users.noreply.github.com>
2026-03-01 23:24:23 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app/app";
+import App from "./app/App";
 import "./styles/index.css";
 import { AuthProvider } from "./app/context/AuthContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
