e2acfd303d34466d7d92aefbc1eccf84807ff105
Matheus Antunes Reis <123640371+matheusantunes-dev@users.noreply.github.com>
2026-03-01 22:09:49 -0300

    Update main.py

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,7 +5,7 @@ from typing import Optional
 import requests
 from jose import jwt
 
-from supabase_storage import SupabaseStorage
+from backend.supabase_storage import SupabaseStorage
 
 
 # =========================
