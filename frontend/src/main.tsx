585ff944960409a1e6edb448c5942301f3db00b9
Matheus Antunes Reis <123640371+matheusantunes-dev@users.noreply.github.com>
2026-03-01 23:32:51 -0300

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,6 @@ import { AuthProvider } from "./app/context/AuthContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID)
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
