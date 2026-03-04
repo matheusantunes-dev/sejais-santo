d583ac8ddea5e71dd36cbc6c3d6050969e91ca9f
Matheus Antunes Reis <123640371+matheusantunes-dev@users.noreply.github.com>
2026-03-04 11:46:17 -0300

    Update App.tsx

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -35,6 +35,25 @@ export default function App() {
   return (
     <>
       <LiturgicalThemeManager />
+      <svg width="0" height="0" style={{ position: "absolute" }}>
+  <filter id="velvet-filter">
+    <feTurbulence
+      type="fractalNoise"
+      baseFrequency="0.5"
+      numOctaves="4"
+      stitchTiles="stitch"
+    />
+    <feColorMatrix
+      type="matrix"
+      values="
+        0 0 0 0 0.5
+        0 0 0 0 0.5
+        0 0 0 0 0.5
+        0 0 0 -0.4 1
+      "
+    />
+  </filter>
+</svg>
 
       <div className="app-container">
         <Header />
